77df1866cdd142b58ee6484efa75164b4464e820
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2017-12-03 20:05:33 +0100

    code cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deployment instructions
+
+- Execute firstuse.sh to setup initial CVE list and the checksum list, prior to first run.
--- cvechecker.py
+++ cvechecker.py
@@ -16,7 +16,6 @@ class Result:
 	
 	def addResult(self, cveid, cvedesc, cvescore, affectedpackages, affectedproducts):
 		if self.resultdict.__contains__(cveid):
-			dosomething=1
 			for pkg in affectedpackages:
 				if not self.resultdict[cveid]['affectedpackages'].__contains__(pkg):
 					self.resultdict[cveid]['affectedpackages'].append(pkg)
@@ -36,7 +35,7 @@ class Result:
 			for pkg in affectedproducts:
 				self.resultdict[cveid]['affectedproducts'].append(pkg)
 
-	def trimResult(self,products=None, packages=None ,scores=None, mute='none'):
+	def trimResult(self,products=None, packages=None ,scores=None, cves=None, mute='none'):
 
 		scoredefs=OrderedDict()
 		scoredefs['None']={'high':0.0, 'low':0.0}
@@ -47,6 +46,14 @@ class Result:
 
 		newresultdict=dict()
 		for key, val in self.resultdict.iteritems():
+
+			if cves != None:
+				match=0
+				for cve in cves:
+					if key == cve:
+						match=1
+				if match == 0:
+					continue
 
 			if scores != None:
 				numfails=0
@@ -62,7 +69,7 @@ class Result:
 				found=0
 				for package in packages:
 					for affpkg in val['affectedpackages']:
-						if package in affpkg:
+						if affpkg.startswith(package):
 							found=1
 							break
 					if found == 1:
@@ -74,7 +81,7 @@ class Result:
 				found=0
 				for product in products:
 					for pdt in val['affectedproducts']:
-						if product in pdt:
+						if pdt.startswith(product):
 							found=1
 							break
 					if found == 1:
@@ -84,6 +91,8 @@ class Result:
 					continue
 
 			newresultdict[key]=val
+
+		#outside the loop
 		if mute != 'none':
 			for entry in newresultdict:
 				newresultdict[entry]['mute']=mute
@@ -113,6 +122,7 @@ class Result:
 				continue
 
 			scorelist.append(val['score'])
+
 			if not cvelist.__contains__(key):
 				cvelist.append(key)
 			for pdt in val['affectedproducts']:
@@ -269,69 +279,6 @@ class CVECheck:
 		if found == 0:
 			print "Word %s does not exist in the corpus"%(tolist)
 
-	def translateWord(self,totrans):
-		self.readStore(self.engjson)
-		decoded=totrans.decode('utf-8')
-		if self.engdict.__contains__(decoded):
-			ctr=1
-			for meaning in self.engdict[decoded]['definitions']:
-				print "%d. %s"%(ctr,meaning)
-				ctr+=1
-			return(0)
-		else:
-			self.readStore(self.corpusjson)
-			for word,refobj in self.mydict.iteritems():
-				for meaning in refobj['definitions']:
-					if totrans == meaning:
-						print "Possible match for %s: %s"%(totrans,word)
-			return(0)
-	
-	def listWord(self,tolist):
-		self.readStore(self.corpusjson)
-		decoded=tolist.decode('utf-8')
-		if self.mydict.__contains__(decoded):
-			print "Word %s exists in the corpus"%(tolist)
-			self.recordLookup(tolist,self.mydict[decoded]['definitions'])
-			ctr=1
-			for meaning in self.mydict[decoded]['definitions']:
-				print "%d. %s"%(ctr,meaning)
-				ctr+=1
-			return(0)
-		else:
-			return(-1)
-		
-	def removeWord(self,toremove):
-		self.readStore(self.corpusjson)
-		decoded=toremove.decode('utf-8')
-		if self.mydict.__contains__(decoded):
-			print "Word %s exists in the corpus"%(decoded)
-			ctr=1
-			for meaning in self.mydict[decoded]['definitions']:
-				print "%d. %s"%(ctr,meaning)
-				ctr+=1
-			resp=raw_input("Press 'R' to remove complete listing, or specify number to delete a specify meaning, any other key to quit.\n")
-			if resp == '':
-				return
-			if resp=='R' or resp=='r':
-				print 'Removing the listing entirely for %s.'%decoded
-				self.mydict.pop(decoded)
-				self.writeStore(self.corpusjson)
-				return
-			intresp=int(resp)
-			if intresp > 0 and intresp <= len(self.mydict[decoded]['definitions']):
-				popped=self.mydict[decoded]['definitions'].pop(intresp-1)
-				print 'Removed %s.'%(popped)
-			else:
-				print 'Ignoring.'
-				return
-			if len(self.mydict[decoded]['definitions']) == 0:
-				self.mydict.pop(decoded)
-				print 'Removed %s as it had no remaining definitions.'%(decoded)
-		else:
-			print "Word %s does not exist in the corpus."%(decoded)
-			return
-		self.writeStore(self.corpusjson)
-
 	def readXdxf(self, dontread=0):
 		if dontread == 0:
 			self.readStore(self.corpusjson)
@@ -383,102 +330,29 @@ class CVECheck:
 		except:
 			print 'Could not write out word corpus or/and xdxf.txt file'
 
-	def recordLookup(self,word,meanings):
-		try:
-			with codecs.open(self.lpjson,'r','utf-8') as infile:
-				self.lpdict=json.load(infile,object_pairs_hook=OrderedDict)
-		except:
-			donothing=1
-		if self.lpdict.__contains__(word.decode('utf-8')):
-			return
-		self.lpdict[word]=meanings
-		with codecs.open(self.lookupout,'w','utf-8') as outfile:
-			for word,meanings in self.lpdict.iteritems():
-				outfile.write(word)
-				outfile.write(':\t')
-				ctr=1
-				for meaning in meanings:
-					outfile.write("%s, "%(meaning))
-					ctr+=1
-				outfile.write('\n')
-		with codecs.open(self.lpjson,'w','utf-8') as outfile:
-			json.dump(self.lpdict,outfile)
-			
-	def writeOuttxtfile(self):
-		self.readStore(self.corpusjson)
-		with codecs.open(self.txtout,'w','utf-8') as outfile:
-			for key, refdict in self.mydict.iteritems():
-				outfile.write(key)
-				outfile.write('\n')
-				dtrncount=1
-				for meaning in refdict['definitions']:
-					outfile.write("\t %d. %s\n"%(dtrncount,meaning))
-					dtrncount+=1
-
-	def addWord(self,toadd):
-		decoded=toadd.decode('utf-8')
-		self.readStore(self.corpusjson)
-		if self.mydict.__contains__(decoded):
-			print "Word %s already exists in the corpus"%(toadd)
-			ctr=1
-			for meaning in self.mydict[decoded]['definitions']:
-				print "%d. %s"%(ctr,meaning)
-				ctr+=1
-			resp=raw_input('Do you wish to add a new meaning(N/y)\n')
-			if resp=='y' or resp=='Y':
-				newm=raw_input('Enter new meaning\n')
-				self.mydict[decoded]['definitions'].append(newm)
-			else:
-				return
-		else:
-			print "Word %s does not exist in the corpus"%(toadd)
-			refdict=OrderedDict()
-			self.mydict[decoded]=self.setupRef(refdict)
-			resp='y'
-			while resp == 'y':
-				newm=raw_input('Enter the meaning for this word\n')
-				self.mydict[decoded]['definitions'].append(newm)
-				resp=raw_input('Do you wish to add another meaning(N/y)\n')
-		self.writeStore(self.corpusjson)
-
-	def listLocalwords(self):
-		self.readStore(self.corpusjson)
-		for key,val in self.mydict.iteritems():
-			if val['source'] == 'local':
-				print "Word %s is present only locally"%(key)
-
-
 aparser=argparse.ArgumentParser(description='A tool to fetch and update a local vulnerability store against select sources of vulnerability information. It can be queried for specific CVEs, by severity or product name, or a combination. Entries can be marked as "seen" to allow one to "mute" alerts for onal words into the corpus.')
 aparser.add_argument("-c", "--cve", type=str, default='none',help='output information about specified CVE or comma-separated list of CVEs. Cannot be combined with any other filter/option.')
-#aparser.add_argument("-u", "--update", type=str, nargs='?',default='none',help='update the store. Cannot be combined with any other option/filter.') #update store
 aparser.add_argument("-s", "--severity", type=str,default='none',help='filter results by severity level. Valid levels are "None", "Low", "Medium", "High", and "Critical".') #lookup by severity level
-aparser.add_argument("-p", "--product", type=str, default='none',help='filter results by specified product name or comma-separated list of products.') #lookup by product
-aparser.add_argument("-pkg", "--package", type=str, default='none',help='filter results by specified product name or comma-separated list of products.') #lookup by product
+aparser.add_argument("-p", "--product", type=str, default='none',help='filter results by specified product name or comma-separated list of products.') #lookup by product like Apache
+aparser.add_argument("-pkg", "--package", type=str, default='none',help='filter results by specified product name or comma-separated list of products.') #lookup by package, e.g. httpd
 aparser.add_argument("-m", "--mute", type=str, default='none',help='set mute on or off, to silence/unsilence reporting. Must be used in combination with one or more filters, and must include -pkg') #mark results as seen or unseen
-aparser.add_argument("-d", "--disp-mute", type=str, nargs='?',default='none',help='display muted entries. Do not use with any other flag.') #mark results as seen or unseen
+aparser.add_argument("-d", "--disp-mute", type=str, nargs='?',default='none',help='display muted entries. Any other options are ignored, when combined with this option.') #mark results as seen or unseen
 
 args=aparser.parse_args()
 cve=args.cve
-#update=args.update
 severity=args.severity
 product=args.product
 package=args.package
 mute=args.mute
 disp_mute=args.disp_mute
+
 argsdict=dict()
 argsdict['scores']=None
 argsdict['products']=None
 argsdict['packages']=None
-
+argsdict['cves']=None
 resobj=Result()
 cvcobj=CVECheck()
-resobj.addResult('101','101 dalmations is the description',10.0,['dalmations'],['Dalmations 101'])
-resobj.addResult('102','James Cameron is the description',4.0,['titanic'],['Titanic the movie'])
-resobj.addResult('102','James Cameron is the description',4.0,['Apache'],['Apache Tomcat'])
-
-if cve != 'none':
-	print 'you have entered a CVE lookup request. Sit tight'
-	sys.exit(0)
 
 if severity != 'none':
 	scores=severity.split(',')
@@ -489,19 +363,17 @@ if severity != 'none':
 	argsdict['scores']=scores
 
 if product != 'none':
-	products=product.split(',')
-	argsdict['products']=products
+	argsdict['products']=product.split(',')
 
 if package != 'none':
-	packages=package.split(',')
-	argsdict['packages']=packages
+	argsdict['packages']=package.split(',')
 
 if mute != 'none':
 	if mute != 'on' and mute != 'off':
 		print 'Value for mute flag can only be "off" or "on"'
 		sys.exit(-1)
-	if package == 'none':
-		print 'Mute flag requires the use of the -pkg option'
+	if package == 'none' and cve == 'none':
+		print 'Mute flag requires the use of the --pkg or the --cve option. If both are specified, --pkg is ignored.'
 		sys.exit(-1)
 
 cvcobj.updateStore()
@@ -509,5 +381,11 @@ if disp_mute != 'none':
 	cvcobj.resObj.printMuted()
 	sys.exit(0)
 
-cvcobj.resObj.trimResult(scores=argsdict['scores'],products=argsdict['products'],packages=argsdict['packages'],mute=mute)
+if cve != 'none':
+	argsdict['cves']=cve.split(',')
+	argsdict['scores']=None
+	argsdict['products']=None
+	argsdict['packages']=None
+
+cvcobj.resObj.trimResult(scores=argsdict['scores'],products=argsdict['products'],packages=argsdict['packages'],cves=argsdict['cves'],mute=mute)
 cvcobj.resObj.printResult(scores=argsdict['scores'],products=argsdict['products'],packages=argsdict['packages'])
