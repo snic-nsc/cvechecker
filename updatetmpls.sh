84e89256cc226a4780501c1bfe8fd124605de9e3
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2017-12-19 18:41:19 +0100

    remove unwanted templates

--- updatetmpls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp CVE-2015.json CVE-2015.json.tmpl
-cp CVE-2016.json CVE-2016.json.tmpl
-cp CVE-2017.json CVE-2017.json.tmpl
-cp CVE-Modified.json CVE-Modified.json.tmpl
-cp nvdchannels.conf nvdchannels.conf.tmpl
-cp redhat-cve.json redhat-cve.json.tmpl
-cp sha256sums sha256sums.tmpl
