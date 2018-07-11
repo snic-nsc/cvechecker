657bc0a58332f50acb22caa663eb1869079345e9
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2018-07-11 15:52:21 +0200

    modify splitreports to output matched-on info in subject, if no other info is available

--- spitsingle.sh
+++ spitsingle.sh
@@ -2,10 +2,7 @@
 
 split_report(){
     tosplit="$1"
-    sender="$2"
-    recips="$3"
-    server="$4"
-    port="$5"
+    cve="$2"
     sl=0
 	lead='---BEGIN REPORT---'
 	end='---END REPORT---'
@@ -21,16 +18,19 @@ split_report(){
 			if echo "$line"|grep -e "$end" >/dev/null; then
 				sl=0;
 				lastcert=${lastcert}$end"";
-                		echo -e "$lastcert"|grep -v 'BEGIN REPORT'|grep -v 'END REPORT' >singlereport
-                		subj=`head -1 singlereport`;
-				status=`grep 'Status:' singlereport|cut -d ' ' -f2`
-                if grep -B2 Nil singlereport |grep Redhat >/dev/null; then
-                        prods=`grep 'Product:' singlereport |cut -d ' ' -f2|paste -sd,`
+				echo $lastcert|grep $cve >/dev/null;
+                if [ $? -eq 0 ]; then
+           			echo -e "$lastcert"|grep -v 'BEGIN REPORT'|grep -v 'END REPORT' >spatout
+                    subj=`head -1 spatout`;
+                    status=`grep 'Status:' spatout|cut -d ' ' -f2`
+                    score=`grep Score spatout|head -1|cut -d '(' -f2|cut -d ')' -f1`
+                    if grep -B2 Nil spatout |grep Redhat >/dev/null; then
+                        prods=`grep 'Product:' spatout |cut -d ' ' -f2|paste -sd,`
                         if [ "$prods" = "" ]; then
-                            matched=`grep 'Matched on' singlereport |cut -d ':' -f2|tr -d ' '`
+                            matched=`grep 'Matched on' spatout |cut -d ':' -f2|tr -d ' '`
                             if [ "$matched" != "" ]; then
                                 subject="${status} ${score} ${subj} Matched on: $matched"
-                            else
+                            else	
                                 subject="${status} ${score} ${subj}"
                             fi
                         else
@@ -39,16 +39,18 @@ split_report(){
                     else
                         subject="${status} ${score} ${subj}"
                     fi
+                    echo $subject
+                    break;
+                fi
 
-				python mailsend.py --sender "$sender" --recips "$recips" --server $server --port $port --subject "$subject" --body singlereport
-			fi;
-			lastcert=${lastcert}$line"\n"; 
-		fi;
-	done
+            fi;
+            lastcert=${lastcert}$line"\n"; 
+        fi;
+    done
 }
 if [ $# -lt 1 ]; then
     echo "Need a file with reports to split. Bailing out";
     exit -1;
 fi
  
-split_report "$1" "$2" "$3" "$4" "$5"
+split_report "$1" "$2"
